a6c861a97914862235094059eec7484ee5af7e76
Andrei Sotan <sotanandrei@gmail.com>
2023-09-05 18:38:10 +0300

    Created basic structure of webapp

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
-import cs50
+import os
 
-print("Hello world")
+from cs50 import SQL
+from datetime import datetime
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finalproject.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
